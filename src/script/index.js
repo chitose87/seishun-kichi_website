70cd5c77a148bf5bfbbbe3df7a55d98e31e14ed7
aokiji516 <my_friends_forever.hiroyuki@docomo.ne.jp>
2018-06-07 20:15:02 +0900

    20180607①（青木）

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,41 @@
 (function () {
-    console.log("aaaa");
+    // console.log("aaaa");
+    // var mv = document.getElementsByClassName("main-visual");
     var mv = document.getElementsByClassName("main-visual");
     console.log(mv);
-})();
+    })
+
+// (function () {
+//     var i = 1;
+//     setInterval(function () {
+//         if(i > 3) i = 1;
+//         $("main-visual").css("background",url('../img/青春"+i+".jpg'));
+//         i++;
+//         },4000
+//     );
+//
+// })
+
+
+// (    i=0;
+//     url = "../img/";
+// )
+// (    var img = new array("青春1.jpg","青春2.png","青春3.jpg");
+//     function change() {
+//         i++;
+//         if(i>=img.length){
+//             i=0;
+//         }
+//         document.body.background = url + img[i];
+//     }
+//     function tm(){
+//         document.body.background = url + img[i];
+//         tm = setInterval("change()",5000);
+//
+//     }
+// )
+
+
+
+
+
