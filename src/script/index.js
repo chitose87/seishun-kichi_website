a067b9d4309be7089b0f5c130d5080ad9bbc7d59
aokiji516 <my_friends_forever.hiroyuki@docomo.ne.jp>
2018-06-09 14:55:31 +0900

    20180609①（青木）

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,128 @@
+var mv = document.getElementById("main-visual");
+
 (function () {
     // console.log("aaaa");
     // var mv = document.getElementsByClassName("main-visual");
-    var mv = document.getElementsByClassName("main-visual");
+    // console.log(document.getElementById("main-visual").style);
+    // var mv = document.getElementById("main-visual");
     console.log(mv);
-    })
+})();
+
+
+
+// img = Array();
+// image[0] = new Image();
+// image[0].src= url("../img/main1.jpg");
+// image[1] = new Image();
+// image[1].src= url("../img/main2.jpg");
+// image[2] = new Image();
+// image[2].src= url("../img/main3.jpg");
+
+// var img = "url('../img/test1.jpg')";
+// var img = "url("../img/main1.jpg")";
+
+img.onload = function(){
+    // mv.style.backgroundImage = "url('../img/main1.jpg')";
+    mv.style.background-image: url("../img/main1.jpg")
+}
+
+
+// document.body.background = "../img/test1.jpg";
+// var mv = document.getElementById("main-visual");
+// var image =
+// document.getElementById("main-visual").style.backgroundImage:'url("../img/test1.jpg")';
+// style.backgroundImage = "url('img_tree.png')";
+// alert("test");
+
+
+// ここから
+//
+// i=0;
+// url = "../img/";
+//
+// img = new Array("../img/main1.jpg","../img/main2.jpg","../img/main3.jpg");
+// function change() {
+//     i++;
+//     if(i>=img.length){
+//         i=0;
+//     }
+//     document.body.background = url + img[i];
+// }
+// function tm(){
+//     document.body.background = url + img[i];
+//     tm = setInterval("change()",3000);
+//
+// }
+
+// ここまで
+
+
+
+
+
+// ここから
+//
+// var url = "../img/";
+//
+// img = new Array("../img/main1.jpg","../img/main2.jpg","../img/main3.jpg");
+//
+// i=0;
+//
+// function change(){
+//     i++;
+//     if (i > img.length) {
+//         i = 0;
+//     }
+//     document.getElementsByClassName("main-visual")[i].style.backgroundImage = url + img[i];
+// }
+//
+// function tm(){
+//     document.getElementsByClassName("main-visual")[i].style.backgroundImage = url + img[i];
+//     tm = setInterval("change()",3000);
+//
+// }
+
+// ここまで
+
+// setInterval("change()",3000);
+
+
+
+// // ここから
+// //
+// (function() {
+//     var i = 1;
+//     setInterval(function() {
+//             if(i > img.length) i = 0;
+//             ("#main-visual").css("background","url('img[i]')");
+//             i++;
+//         }, 4000
+//     );
+// });
+//
+// // ここまで
+
+// ここから
+//
+// i=0;
+//
+// function change(){
+//     i++;
+//     if (i > img.length) {
+//         i = 0;
+//     }
+//     ("#main-visual").css("background","url('img[i]')");
+//     };
+//
+// function tm(){
+//     ("#main-visual").css("background","url('img[i]')");
+//     };
+//     tm = setInterval("change()",3000);
+//
+// img.onload = tm;
+
+// ここまで
+
 
 // (function () {
 //     var i = 1;
